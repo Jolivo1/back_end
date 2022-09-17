4136adeed2b6cbeed14ddb74801a67e4440eff41
Joaquin Olivo <joaquin.olivo@hotmail.com>
2022-09-17 10:43:20 -0500

    experss

--- mystore/index.js
+++ mystore/index.js
@@ -3,8 +3,14 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    console.log(req)
+  res.send(req)
 })
+
+app.post('/', (req, res) => {
+    res.send('Hello World two!')
+  })
+  
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
